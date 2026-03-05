f12ead29e1fb19f4c5b301af61260480d1400fd8
ketsusi <72655050+ketsusi@users.noreply.github.com>
2026-03-05 20:28:35 +0800

    Update Rust file path and improve echo messages

--- .github/workflows/Scripts/Handles.sh
+++ .github/workflows/Scripts/Handles.sh
@@ -3,12 +3,14 @@
 PKG_PATH="$GITHUB_WORKSPACE/openwrt-src/package/"
 
 #修复Rust编译失败
-RUST_FILE=$(find ../feeds/packages/ -maxdepth 3 -type f -wholename "*/rust/Makefile")
+#RUST_FILE=$(find ../feeds/packages/ -maxdepth 3 -type f -wholename "*/rust/Makefile")
+RUST_FILE=$(find $GITHUB_WORKSPACE/openwrt-src/feeds/packages/ -maxdepth 3 -type f -wholename "*/rust/Makefile")
 if [ -f "$RUST_FILE" ]; then
- 	echo " "
+    echo "Fixing rust build..."
 
- 	sed -i 's/ci-llvm=true/ci-llvm=false/g' $RUST_FILE
-	sed -i 's/download-ci-llvm = true/download-ci-llvm = false/g' $RUST_FILE
+    sed -i 's/ci-llvm=true/ci-llvm=false/g' $RUST_FILE
+    sed -i 's/download-ci-llvm = true/download-ci-llvm = false/g' $RUST_FILE
 
-	cd $PKG_PATH && echo "rust has been fixed!"
+    cd $PKG_PATH
+    echo "rust has been fixed!"
 fi
